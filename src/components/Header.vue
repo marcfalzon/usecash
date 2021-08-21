da523ece590ad92fbea06bbc879fe1651c8136f1
Modenero <support@modenero.com>
2021-08-20 20:02:24 -0400

    Update header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <nav class="bg-gray-800">
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <div class="hidden absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                     <button @click="open" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
@@ -21,11 +21,12 @@
                     </button>
                 </div>
 
-                <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start cursor-pointer">
+                <!-- <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start cursor-pointer"> -->
+                <div class="flex-1 flex items-stretch justify-start cursor-pointer">
                     <div class="flex-shrink-0 flex items-center" @click="home">
-                        <img class="block lg:hidden h-8 w-auto" src="@/assets/bitcoin-cash-bch-logo.svg" alt="Use.cash">
-                        <!-- <img class="hidden lg:block h-8 w-auto" src="@/assets/workflow-logo-indigo-500-mark-white-text.svg" alt="Use.cash"> -->
-                        <img class="hidden lg:block h-8 w-auto" src="@/assets/use-cash-logo.svg" alt="Use.cash">
+                        <!-- <img class="block lg:hidden h-8 w-auto" src="@/assets/bitcoin-cash-bch-logo.svg" alt="Use.cash">
+                        <img class="hidden lg:block h-8 w-auto" src="@/assets/use-cash-logo.svg" alt="Use.cash"> -->
+                        <img class="block h-8 w-auto" src="@/assets/use-cash-logo.svg" alt="Use.cash">
                     </div>
                 </div>
 
@@ -83,7 +84,7 @@
         </div>
 
         <!-- Mobile menu, show/hide based on menu state. -->
-        <div class="sm:hidden" id="mobile-menu">
+        <div class="hidden sm:hidden" id="mobile-menu">
             <div class="px-2 pt-2 pb-3 space-y-1">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="javascript://" :class="unselected" aria-current="page" @click="add">ADD MERCHANT</a>
