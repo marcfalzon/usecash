89705519f0099494eaa5aec05980528c1920b00a
Modenero <support@modenero.com>
2022-05-22 20:56:09 -0400

    Update API geo (lon) filter.

--- api/routes/mapBounds.js
+++ api/routes/mapBounds.js
@@ -18,6 +18,9 @@ const mapBounds = async function (req, res) {
     let endpoint
     let results
 
+    let merchants = []
+    let venues = []
+
     /* Set body. */
     let body = req.body
     console.log('BODY', body, typeof body)
@@ -51,9 +54,9 @@ const mapBounds = async function (req, res) {
     const category = body.cat
 
     /* Request coinmap venues. */
-    results = await coinmapDb.query('api/byGeo', {
-        startkey: [ parseFloat(sw.lat) ],
-        endkey: [ parseFloat(ne.lat) ],
+    results = await coinmapDb.query('api/byLat', {
+        startkey: parseFloat(sw.lat),
+        endkey: parseFloat(ne.lat),
         include_docs: true,
     })
     .catch(err => {
@@ -69,16 +72,27 @@ const mapBounds = async function (req, res) {
         return res.json([])
     }
 
-    /* Set venues. */
-    const venues = results.rows.map(_venue => {
+    console.log(`\nFound [ ${results.rows.length} ] venues matching that LAT`)
+
+    /* Filter venues. */
+    venues = results.rows.filter(_venue => {
         /* Set venue. */
         const venue = _venue.doc
 
+        /* Parse longitude. */
+        if (venue.lon >= parseFloat(sw.lng) && venue.lon <= parseFloat(ne.lng)) {
+            return venue
+        }
+    })
+
+    /* Update venues. */
+    // NOTE: Added to improve data compatibility.
+    venues = venues.map(_venue => {
         /* Add (alt) longitude. */
-        venue.lng = venue.lon
+        _venue.lng = _venue.lon
 
         /* Return venue. */
-        return venue
+        return _venue
     })
     console.log('FOUND', venues.length, 'venues')
 
@@ -95,15 +109,17 @@ const mapBounds = async function (req, res) {
         console.error('SESSION ERROR:', err)
     })
 
-    /* Set merchants. */
-    const merchants = results.rows.map(_merchant => {
-        /* Set merchant. */
-        const merchant = _merchant.doc
+    if (results) {
+        /* Set merchants. */
+        merchants = results.rows.map(_merchant => {
+            /* Set merchant. */
+            const merchant = _merchant.doc
 
-        /* Return merchant. */
-        return merchant
-    })
-    console.log('FOUND', merchants.length, 'merchants')
+            /* Return merchant. */
+            return merchant
+        })
+        console.log('FOUND', merchants.length, 'merchants')
+    }
 
     /* Return JSON. */
     return res.json([
