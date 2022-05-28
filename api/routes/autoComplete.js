e0de08b8758f77e4b0dd21dffbf47da778d9b9ba
Modenero <support@modenero.com>
2022-05-28 00:23:54 -0400

    Enable auto-complete query via (slow) CouchDB.

--- api/routes/autoComplete.js
+++ api/routes/autoComplete.js
@@ -1,22 +1,20 @@
 /* Import modules. */
-const Airtable = require('airtable')
 const moment = require('moment')
+const PouchDB = require('pouchdb')
 const superagent = require('superagent')
 const util = require('util')
 
-/* Set refresh delay. */
-const SECONDS_TO_REFRESH = 30
-
-/* Set refresh delay. */
-// FIXME: Set to the maximum supported by Airtable
-const MAX_RECORDS = 10000
+/* Initialize databases. */
+// const logsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/logs`)
+// const sessionsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/sessions`)
+const coinmapDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/coinmap`)
 
 /**
  * Auto Complete
  */
 const autoComplete = async function (req, res) {
     /* Set query. */
-    const query = req.params.query
+    const query = req.params.query.toLowerCase()
     console.log('QUERY', query)
 
     /* Validate query. */
@@ -32,176 +30,42 @@ const autoComplete = async function (req, res) {
 
     let index
     let type
-    let response
+    let results
     let body
     let dslQuery
     let hits
 
-    /* Initialize fields. */
-    // NOTE: Used by multi_match.
-    let fields
-
-    /* Set index. */
-    index = 'coinmap'
-
-    /* Set (search) type. */
-    type = 'phrase_prefix'
-
-    /* Set endpoint. */
-    endpoint = `http://127.0.0.1:9200/${index}/_search`
-
-    /* Set fields. */
-    fields = ['name', 'companyName', 'streetAddress', 'neighborhood', 'city', 'state']
-
-    const category = 'atm'
-
-    /* Build DSL query. */
-    // const dslQuery = {
-    //     from: 0,
-    //     size: 15,
-    //     query: {
-    //         multi_match: { fields, query, type }
-    //     }
-    // }
-    dslQuery = {
-        from: 0,
-        size: 15,
-        query: {
-            bool: {
-                must: [
-                    {
-                        multi_match: { fields, query, type }
-                    },
-                    {
-                        term: {
-                            category
-                        }
-                    },
-                ],
-            }
-        }
-    }
-    console.log('DSL QUERY', util.inspect(dslQuery, false, null, true))
-
-    /* Request Elasticsearch query. */
-    response = await superagent
-        .post(endpoint)
-        .send(dslQuery)
-        .set('accept', 'json')
-    // console.log('RESPONSE', response)
-
-    /* Validate body. */
-    if (response.body) {
-        /* Set response body. */
-        body = response.body
-    } else {
-        /* Validate response text. */
-        if (!response.text) {
-            return
-        }
-
-        try {
-            /* Parse response text. */
-            body = JSON.parse(response.text)
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    hits = body.hits.hits
-    console.log('HITS', hits)
-
-    /* Set index. */
-    index = 'merchants'
-
-    /* Set (search) type. */
-    type = 'phrase_prefix'
-
-    /* Set endpoint. */
-    endpoint = `http://127.0.0.1:9200/${index}/_search`
-
-    /* Set fields. */
-    fields = ['name', 'companyName', 'streetAddress', 'neighborhood', 'city', 'state']
-
-    /* Build DSL query. */
-    dslQuery = {
-        from: 0,
-        size: 15,
-        query: {
-            multi_match: { fields, query, type }
-        }
-    }
-    console.log('DSL QUERY', util.inspect(dslQuery, false, null, true))
-
-    /* Request Elasticsearch query. */
-    response = await superagent
-        .post(endpoint)
-        .send(dslQuery)
-        .set('accept', 'json')
-    // console.log('RESPONSE', response)
-
-    /* Validate body. */
-    if (response.body) {
-        /* Set response body. */
-        body = response.body
-    } else {
-        /* Validate response text. */
-        if (!response.text) {
-            return
-        }
-
-        try {
-            /* Parse response text. */
-            body = JSON.parse(response.text)
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    /* Validate body. */
-    if (body) {
-        /* Set (search request) hits. */
-        const hits = body.hits
-
-        /* Validate hits. */
-        if (hits.hits) {
-            body = hits.hits
-        }
-    }
-
-    hits = [
-        ...hits,
-        ...body,
-    ]
-
-    /* Initialize package. */
-    let pkg = {}
-
-    /* Initialize keywords. */
-    pkg.keywords = []
-
-    /* Add keywords. */
-    body.forEach(hit => {
-        if (hit._source.name || hit._source.companyName) {
-            pkg.keywords.push(hit._source.name || hit._source.companyName)
-        }
+    /* Request existing user. */
+    results = await coinmapDb.query('api/byKeyword', {
+        // include_docs: true,
+    }).catch(err => {
+        console.error('DATA ERROR:', err)
     })
+    // console.log('USERS RESULT (byKeyword)', util.inspect(results, false, null, true))
 
-    /* Remove duplicates. */
-    if (pkg.keywords.length) {
-        pkg.keywords = [...new Set(pkg.keywords)]
+    let venues
+    let count = 0
+
+    if (results.rows.length > 0) {
+        venues = results.rows.filter(_venue => {
+            const key = _venue.key.toLowerCase()
+
+            if (key.indexOf(query) !== -1) {
+                // console.log('KEY', key)
+                count++
+                return true
+            } else {
+                return false
+            }
+        })
+
     }
-
-    pkg.locations = [
-        'Canada',
-        'Quebec',
-    ]
-
-    // pkg.hits = body
-    pkg.hits = hits
 
     /* Return package. */
-    res.json(pkg)
+    res.json({
+        count,
+        venues,
+    })
 }
 
 /* Export module. */
